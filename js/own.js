d3dfcbde2937575c5ce7961e1aad4438f790e96d
841148070 <841148070@qq.com>
2017-12-01 20:23:12 +0800

    first commit

---
+++ js/own.js
@@ -0,0 +1,66 @@
+
+//点击改变主题颜色
+$(function(){
+	$('.change span').click(function(){
+		var col=$(this).attr('title');
+		$('nav li').hover(function(){
+			$(this).css('background',col);
+		},function(){
+			$(this).css('background','transparent');
+		})
+		$('.col').css('background',col);
+	})
+})
+
+//点击展开,收回
+$(function(){
+	$('.newstyle .left h3').click(function(){
+		if($(this).next().css('display')=='none'){
+			$(this).next().css('display','block')
+			.prev().children('span').css({'transform':'rotate(-45deg)','top':'26px'})
+		}else{
+			$(this).next().css('display','none')
+			.prev().children('span').css({'transform':'rotate(135deg)','top':'20px'})
+		}
+	})
+})
+//轮播
+$(function(){
+	$('.v_show ul').clone(true).appendTo('.v_show');
+	
+//	var liw=$('.v_show li').width()+30;
+	var page=$('.v_show li').length;
+	var num=1;
+	$('.next').click(function(){
+		if(num==(page/2)){
+			$('.v_show').stop().animate({left:'30'-400*num},300)
+				.animate({left:'30'},1);
+			num=1;
+		}else{
+			$('.v_show').stop().animate({left:'30'-400*num},300);
+			num+=1;
+		}
+	})
+	$('.pre').click(function(){
+		if(num==1){
+			$('.v_show').stop().animate({left:'30'-400*3},1)
+				.animate({left:'30'-400*2},300);
+			num=page/2;
+		}else{
+			$('.v_show').stop().animate({left:'30'-400*(num-2)},300);
+			num-=1;
+		}
+	})
+})
+
+
+//info 切换
+$(function(){
+	$('.info ul li').click(function(){
+		var num=$(this).index()+1;
+		$('.info ul li').removeClass('active').parent().siblings().removeClass('on');
+		$(this).addClass('active');
+		$('.info div:nth-of-type('+num+")").addClass('on');
+		
+	})
+})
---
+++ js/switchColor.js
@@ -0,0 +1,19 @@
+﻿/*衣服颜色切换*/
+$(function(){
+	$(".color_change ul li img").click(function(){           
+		  var imgSrc = $(this).attr("src");  //小图路径
+		  var i = imgSrc.lastIndexOf(".");  //小图的索引（位置）
+		  var unit = imgSrc.substring(i);   //小图拓展名
+		  imgSrc = imgSrc.substring(0,i);   //小图主名
+		  var imgSrc_small = imgSrc + "_one_small"+ unit;    //蓝色小图
+		  var imgSrc_big = imgSrc + "_one_big"+ unit;   //蓝色大图
+		  $("#bigImg").attr({"src": imgSrc_small ,"jqimg": imgSrc_big });   //大图路径
+		  $("#thickImg").attr("href", imgSrc_big);   
+		  var alt = $(this).attr("alt");
+		  $(".color_change strong").text(alt);
+		  var url = imgSrc+".html";
+		  $(".pro_detail_left ul.imgList")
+				.empty()
+				.load(url);
+	});
+});
